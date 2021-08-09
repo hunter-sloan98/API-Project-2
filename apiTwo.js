bc3ddd23053cbe1f02096873a8aa5415a404482c
hunter-sloan98 <sloan.hunter98@gmail.com>
2021-08-09 12:55:40 -0400

    Visuals & Basic Layout

--- apiTwo.js
+++ apiTwo.js
@@ -1,1 +1,3 @@
-'API Key' = 13d385a84dbdd7460c1a716f2e70fcfb
+'API Key' = "13d385a84dbdd7460c1a716f2e70fcfb"
+
+let baseUrl = "http://api.coinlayer.com/api/live?access_key=13d385a84dbdd7460c1a716f2e70fcfb"
