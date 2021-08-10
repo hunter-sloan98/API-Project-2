7c37e40cfc4fa54bae0f03709398eff848dcfeac
hunter-sloan98 <sloan.hunter98@gmail.com>
2021-08-10 12:21:08 -0400

    Complete Final Push

--- apiTwo.js
+++ apiTwo.js
@@ -1,3 +1,21 @@
-'API Key' = "13d385a84dbdd7460c1a716f2e70fcfb"
+// 'API Key' = "13d385a84dbdd7460c1a716f2e70fcfb"
+let url = "http://api.coinlayer.com/api/live?access_key=13d385a84dbdd7460c1a716f2e70fcfb"
 
-let baseUrl = "http://api.coinlayer.com/api/live?access_key=13d385a84dbdd7460c1a716f2e70fcfb"
+document.getElementById("searchBtn").onclick = function(){
+  let searchValue = document.getElementById('cryptoSearch').value
+  fetch(url)
+  .then(function(response){
+    return response.json();
+  })
+  .then(function(json){
+    // console.log('displayRates:', json);
+    Object.entries(json)
+    let myRates = json.rates[searchValue]
+    console.log(myRates)
+    let d = document.createElement('li')
+    d.innerHTML = `Current rate for ${searchValue} is $${myRates}`;
+    document.getElementById('myList').appendChild(d)
+    
+  })
+  }
+
